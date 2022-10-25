389dbc1b1d1dc1238ee25a9ea48ba41bc34739d4
ZyadBagus <zyadbagus.1@gmail.com>
2022-10-25 10:34:25 +0700

    pertemuan 6

--- application/views/view-data-matakuliah.php
+++ application/views/view-data-matakuliah.php
@@ -19,7 +19,32 @@
 			</tr>
 			<tr>
 				<th>Kode MTK</th>
-				<th>:</>
+				<th>:</th>
 				<td>
 					<?= $kode; ?>
+				</td>
 			</tr>
+			<tr>
+				<th>Nama MTK</th>
+				<th>:</th>
+				<td>
+					<?= $nama; ?>
+				</td>
+			</tr>
+			<tr>
+				<th>SKS</th>
+				<th>:</th>
+				<td>
+					<?= $sks; ?>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="3" align="center">
+					<a href="<?= base_url('matakuliah');?>">Kembali</a>
+				</td>
+			</tr>
+		</table>
+	</center>
+</body>
+
+</html>
--- application/views/view-form-matakuliah.php
+++ application/views/view-form-matakuliah.php
@@ -6,7 +6,7 @@
 
 <body>
 	<center>
-		<form> action="<?= base_url('matakuliah/cetak'); ?>" method="post">
+		<form action="<?= base_url('matakuliah/cetak'); ?>" method="post">
 			<table>
 				<tr>
 					<th colspan="3">
@@ -20,28 +20,49 @@
 				</tr>
 				<tr>
 					<th>Kode MTK</th>
-					<th>:</th>
+					<td>:</td>
 					<td>
-						<input type="text" name="kode" id="kode">
+						<input type="text" name="kode" id="kode" value="<?= set_value('kode'); ?>">
+					</td>
+				</tr>
+				<tr>
+					<th></th>
+					<th></th>
+					<td colspan="3">
+						<font style="color:red; font-size:10; font-align:center;"><?= form_error('kode'); ?>
 					</td>
 				</tr>
 				<tr>
 					<th>Nama MTK</th>
-					<th>:</th>
+					<td>:</td>
 					<td>
-						<input type="text" name="nama" id="nama">
+						<input type="text" name="nama" id="nama"  value="<?= set_value('nama'); ?>">
+					</td>
+				</tr>
+				<tr>
+					<th></th>
+					<th></th>
+					<td colspan="3">
+						<font style="color:red; font-size:10; font-align:center;"><?= form_error('nama'); ?>
 					</td>
 				</tr>
 				<tr>
 					<th>SKS</th>
-					<th>:</th>
+					<td>:</td>
 					<td>
-						<select name="sks" id="sks">
-							<option value="">Pilih SKS</option>
-							<option value="2">2</option>
-							<option value="3">3</option>
-							<option value="4">4</option>
+						<select name="sks" >
+							<option value=""<?= set_select('sks','', TRUE); ?>>Pilih SKS</option>
+							<option value="2"<?= set_select('sks', '2'); ?>>2</option>
+							<option value="3"<?= set_select('sks', '3'); ?>>3</option>
+							<option value="4"<?= set_select('sks', '4'); ?>>4</option>
 						</select>
+					</td>
+				</tr>
+				<tr>
+					<th></th>
+					<th></th>
+					<td colspan="3">
+						<font style="color:red; font-size:10; font-align:center;"><?= form_error('sks'); ?>
 					</td>
 				</tr>
 				<tr>
